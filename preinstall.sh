b85b6774de31b1e43e6c3ee0088ee8c76c390a8c
cipherodio <cipherodio@gmail.com>
2026-02-13 17:46:40 +0800

    Mirror update

--- preinstall.sh
+++ preinstall.sh
@@ -60,8 +60,8 @@ mount --mkdir -o rw,noatime "${DATA_DISK}p1" /mnt/data
 msg "Setting mirrorlist"
 
 cat >/etc/pacman.d/mirrorlist <<'EOF'
-Server = https://mirror.sg.gs/archlinux/$repo/os/$arch
-Server = http://mirror.xtom.com.hk/archlinux/$repo/os/$arch
+Server = https://sg.mirrors.cicku.me/archlinux/$repo/os/$arch
+Server = https://hk.mirrors.cicku.me/archlinux/$repo/os/$arch
 Server = https://singapore.mirror.pkgbuild.com/archlinux/$repo/os/$arch
 EOF
 
