46702c5d916de3235e8d55856616a355e1847931
cipherodio <cipherodio@gmail.com>
2026-02-13 00:38:17 +0000

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -75,9 +75,11 @@ msg "Generating fstab"
 
 genfstab -U /mnt >>/mnt/etc/fstab
 
-# Fix mount options
-sed -i '/\/efi/s/defaults/defaults,fmask=0077,dmask=0077/' /mnt/etc/fstab
-sed -i '/\/data/s/defaults/defaults,noatime/' /mnt/etc/fstab
+# Secure EFI mount
+sed -i '/\/efi/ s|\(vfat[[:space:]]*\)[^[:space:]]*|\1rw,relatime,fmask=0077,dmask=0077|' /mnt/etc/fstab
+
+# Data performance tweak
+sed -i '/\/data/ s|\(ext4[[:space:]]*\)[^[:space:]]*|\1rw,noatime|' /mnt/etc/fstab
 
 msg "Preinstall complete."
 msg "Now run: arch-chroot /mnt"
