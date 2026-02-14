0a3ccdfe62e39022fabd08053af4e54d64bd00d8
cipherodio <cipherodio@gmail.com>
2026-02-14 12:15:58 +0800

    Test

--- test.sh
+++ test.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# RUn:
-# USER_PASSWORD='MyTestPass123' curl -fsSL https://gitlab.com/cipherodio/archstrap/-/raw/main/test.sh | bash
-
 set -Eeuo pipefail
 
 msg() { printf "\033[1;94m==>\033[0m %s\n" "$1"; }
@@ -10,20 +7,24 @@ die() {
     exit 1
 }
 
-USERNAME="slutshit"
+USERNAME="gptgay"
 
 # -------------------------
-# REQUIRE PASSWORD
+# Check required env vars
 # -------------------------
-
-if [ -z "${USER_PASSWORD:-}" ]; then
-    die "USER_PASSWORD is not set. Use: USER_PASSWORD='mypassword' $0"
-fi
+[ -z "${ROOT_PASSWORD:-}" ] && die "ROOT_PASSWORD is not set. Use: ROOT_PASSWORD='…' bash"
+[ -z "${USER_PASSWORD:-}" ] && die "USER_PASSWORD is not set. Use: USER_PASSWORD='…' bash"
 
 # -------------------------
-# CREATE USER
+# Set root password
 # -------------------------
+msg "Setting root password"
+echo "root:${ROOT_PASSWORD}" | chpasswd
+msg "Root password set successfully!"
 
+# -------------------------
+# Create user
+# -------------------------
 msg "Creating user '$USERNAME' if it doesn't exist"
 if ! id -u "$USERNAME" >/dev/null 2>&1; then
     useradd -m -G wheel,video,audio,storage,power,input,render -s /bin/zsh "$USERNAME"
@@ -32,20 +33,13 @@ else
     msg "User '$USERNAME' already exists"
 fi
 
-# -------------------------
-# SET PASSWORD
-# -------------------------
-
+# Set user password
 msg "Setting password for '$USERNAME'"
-echo "${USERNAME}:${USER_PASSWORD}" | chpasswd || die "Failed to set password"
+echo "${USERNAME}:${USER_PASSWORD}" | chpasswd
+msg "User password set successfully!"
 
-# -------------------------
-# SET SUDOERS
-# -------------------------
-
-SUDO_FILE="/etc/sudoers.d/00_${USERNAME}"
+# Configure sudoers
 msg "Configuring sudoers for '$USERNAME'"
-echo "${USERNAME} ALL=(ALL) ALL" >"$SUDO_FILE"
-chmod 440 "$SUDO_FILE"
-
-msg "Done! User '$USERNAME' is ready."
+echo "${USERNAME} ALL=(ALL) ALL" >/etc/sudoers.d/00_"$USERNAME"
+chmod 440 /etc/sudoers.d/00_"$USERNAME"
+msg "Sudoers configured!"
